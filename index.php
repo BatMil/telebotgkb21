e423ad3133c5d79847991879a0a664b269ed1ed5
BatMil <milyash_93@mail.ru>
2017-11-16 13:16:31 +0500

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ $output=(file_get_contents('php://input'));
 //$id=$output['message']['chat']['id'];
 //file_get_contents("https://api.telegram.org/bot492898194:AAE_gQQobpTAK_54TJ74ljpC1Ou1lcH8zNM/sendMessage?chat_id=".$id."&text=i see you");
 file_put_contents("logs.txt", $output);
-//echo $output;
+echo $output;
 ?>
