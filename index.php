a0b0cb613770f3e73e02b451f7adf3b4a594a7c5
Миляуша Батргареева <Batrgareeva@B21.LOCAL>
2017-11-17 15:21:59 +0500

    a

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-$output=(file_get_contents('php://input'));
+$output=(file_get_contents('https://api.telegram.org/bot492898194:AAE_gQQobpTAK_54TJ74ljpC1Ou1lcH8zNM/getupdates'));
 //$id=$output['message']['chat']['id'];
 //file_put_contents("https://api.telegram.org/bot492898194:AAE_gQQobpTAK_54TJ74ljpC1Ou1lcH8zNM/sendMessage?chat_id=".$id."&text=i see you");
 //echo $output);
