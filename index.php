fa1ca3966aaef934e719f342681311cf03c42eb1
BatMil <milyash_93@mail.ru>
2017-11-17 08:29:26 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 $output=(file_get_contents('php://input'));
 //$id=$output['message']['chat']['id'];
-//file_get_contents("https://api.telegram.org/bot492898194:AAE_gQQobpTAK_54TJ74ljpC1Ou1lcH8zNM/sendMessage?chat_id=".$id."&text=i see you");
+file_put_contents("https://api.telegram.org/bot492898194:AAE_gQQobpTAK_54TJ74ljpC1Ou1lcH8zNM/sendMessage?chat_id=".$id."&text=i see you");
 file_put_contents("logs.txt", $output);
 echo $output;
 ?>
